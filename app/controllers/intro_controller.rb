ecedcd1497804b5d675ab25e40c066f03f8fd1a5
Iwasaki-Y0125 <dcmmw012@gmail.com>
2025-09-18 12:32:51 +0900

    Closes #19 PC上でも縦サイズをスマホサイズ（667px）に調整

--- app/controllers/intro_controller.rb
+++ app/controllers/intro_controller.rb
@@ -1,2 +1,18 @@
 class IntroController < ApplicationController
+  def index; end
+
+  def step
+    @step = params[:step].to_i
+
+    case @step
+    when 1
+      render partial: 'step1'
+    when 2
+      render partial: 'step2'  
+    when 3
+      render partial: 'step3'
+    else
+      head :not_found
+    end
+  end
 end
