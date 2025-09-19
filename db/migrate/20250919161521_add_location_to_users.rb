0ec5ebdd7660720d6ef7b4eaf0ebb7597ab89fc2
Iwasaki-Y0125 <dcmmw012@gmail.com>
2025-09-20 02:18:31 +0900

    fixed Lintエラー

--- db/migrate/20250919161521_add_location_to_users.rb
+++ db/migrate/20250919161521_add_location_to_users.rb
@@ -4,6 +4,6 @@ class AddLocationToUsers < ActiveRecord::Migration[7.2]
     add_column :users, :longitude, :decimal, precision: 10, scale: 7
 
     # 位置情報での検索用インデックス
-    add_index :users, [:latitude, :longitude], name: 'index_users_on_location'
+    add_index :users, [ :latitude, :longitude ], name: 'index_users_on_location'
   end
 end
